83f3600daca981a9ddf53c370a5fca5df1c06d3e
Santiago Nahmod <santiago.nahmod@widergy.com>
2022-07-17 20:15:00 -0300

    Add Rankings Controller

--- app/serializers/profile_votes_aggregation_serializer.rb
+++ app/serializers/profile_votes_aggregation_serializer.rb
@@ -1,3 +1,4 @@
 class ProfileVotesAggregationSerializer < ActiveModel::Serializer
-  attributes :votes_count, :rat_votes_count, :frog_votes_count, :rat_votes_ratio, :frog_votes_ratio
+  attributes :votes_count, :rat_votes_count, :frog_votes_count, :rat_votes_ratio,
+             :frog_votes_ratio, :votes_ratio
 end
